65d62f2d62fd7681f13a9fd9a08a43a06ab50693
Trayan Azarov <trayan.azarov@amikos.tech>
2023-07-31 18:21:39 +0300

    fix: Fixed an issue with image

--- tests/test_chroma.py
+++ tests/test_chroma.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import uuid
 
@@ -7,7 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 def get_embedding_function():
     """
     Get the embedding function
@@ -28,12 +30,16 @@ sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFuncti
 def test_chroma():
     client = chromadb.HttpClient(host="localhost", port=8000)
     client.heartbeat()
-    client.reset()
+    # client.reset()
     collection = client.get_or_create_collection("all1-my-documents",
                                                  embedding_function=sentence_transformer_ef)
     collection.add(documents=["this is a test embedding"], metadatas=[{"type": "page"}], ids=[str(uuid.uuid4())])
     assert len(collection.get()['ids']) == 1
 
+def test_reset():
+    client = chromadb.HttpClient(host="localhost", port=8000)
+    client.heartbeat()
+    client.reset()
 
 if __name__ == '__main__':
     test_chroma()
