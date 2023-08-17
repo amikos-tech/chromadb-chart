68fa45fa93fa8241bee8ece84e4c3fc07a90da1c
Trayan Azarov <trayan.azarov@amikos.tech>
2023-08-17 16:27:46 +0300

    fix: chroma-hnswlib reinstall is needed as it does uniformly work across platforms.

--- image/docker_entrypoint.sh
+++ image/docker_entrypoint.sh
@@ -3,5 +3,5 @@ export CHROMA_SERVER_HTTP_PORT=${CHROMA_SERVER_HTTP_PORT:-8000}
 export CHROMA_SERVER_LOG_CONFIG=${CHROMA_SERVER_LOG_CONFIG:-log_config.yaml}
 export CHROMA_SERVER_HOST=${CHROMA_SERVER_HOST:-"0.0.0.0"}
 . /chroma/venv/bin/activate
-#pip install --force-reinstall --no-cache-dir chroma-hnswlib
+pip install --force-reinstall --no-cache-dir chroma-hnswlib
 uvicorn chromadb.app:app --workers 1 --host ${CHROMA_SERVER_HOST} --port ${CHROMA_SERVER_HTTP_PORT} --proxy-headers --log-config ${CHROMA_SERVER_LOG_CONFIG}
